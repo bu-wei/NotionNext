df39577fc1e39ad030e55752cb392e73fea6861e
bu-wei <yuefuqinxin@gmail.com>
2023-04-12 05:11:45 +0800

    back2top

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -25,9 +25,9 @@ const CONFIG_NEXT = {
   MENU_ARCHIVE: true, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
-  WIDGET_TO_TOP: false, // 是否显示回顶
-  WIDGET_TO_BOTTOM: true, // 显示回底
-  WIDGET_DARK_MODE: true, // 显示日间/夜间模式切换
+  WIDGET_TO_TOP: true, // 是否显示回顶
+  WIDGET_TO_BOTTOM: false, // 显示回底
+  WIDGET_DARK_MODE: false, // 显示日间/夜间模式切换
   WIDGET_TOC: true, // 移动端显示悬浮目录
 
   ARTICLE_SHARE: process.env.NEXT_PUBLIC_ARTICLE_SHARE || false, // 文章分享功能
